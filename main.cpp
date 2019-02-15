0d0d6f716b53b6c1b8ee519dfba3801908529148
Sina Pilehchiha <46059077+sinathemountain@users.noreply.github.com>
2019-02-14 19:30:58 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,9 +8,8 @@ int main(int argc, char* argv[]) {
     std::string outFile = argv[2]; //    this argument is also given to the executable pogram via the command line interface.
     int  bufferSize = 2000;      // allow the sorter to use 2Kb (base 10) of memory for sorting.
     //  once full, it will dump to a temp file and grab another chunk.
-    bool compressOutput = false;       //   not yet supported
     std::string tempPath     = "./";        //  allows you to write the intermediate files anywhere you want.
-    KwayMergeSort *claim_sorter = new KwayMergeSort (inFile, outFile, bufferSize, compressOutput, tempPath); //  create a new instance of the KwayMergeSort class.
+    KwayMergeSort *claim_sorter = new KwayMergeSort (inFile, outFile, bufferSize, tempPath); //  create a new instance of the KwayMergeSort class.
     const clock_t BEGINNING = clock(); //   mark the beginning of the execution of the sorting procedure
     claim_sorter->Sort(); //    this is the main call to the sorting procedure.
     const double EXECUTION_TIME = (double)(clock() - BEGINNING) / CLOCKS_PER_SEC / 60; //   report the execution time (in minutes)
